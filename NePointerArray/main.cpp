0bed18765d8e7c7d7cd5ebb6872bc2093217812f
tianyi <styzf@qq.com>
2019-11-18 17:14:00 +0800

    Initial Commit

---
+++ NePointerArray/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NePointerArray
+//
+//  Created by 天涯路 on 2019/11/18.
+//  Copyright © 2019年 天涯路. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
